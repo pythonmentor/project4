bbd6e8af5ec5468c463abb5de26c90bad0d31b41
FlavienCodeur <92309006+FlavienCodeur@users.noreply.github.com>
2022-01-25 08:52:14 +0100

    Add files via upload

---
+++ vues/menup.py
@@ -0,0 +1,10 @@
+def menu() -> str:
+    return input(
+        "Bonjour, Bienvenue sur le menu pour manager un tournoi d'echecs.\n"
+        "Que puis je faire pour vous ?\n"
+        "(OPTION) Taper pour modifier le niveau d'un joueur\n"
+        "(OPTION) Taper pour creer un tournoi\n"
+        "(OPTION) Taper pour commencer ou continuer un tournoi\n"
+        "(OPTION) Taper pour montrer tout les tournois\n"
+        "(OPTION) Taper pour montreer tout les joueurs\n"
+        "(OPTION) Taper pour quitter le programme\n")
---
+++ vues/menutournoi.py
@@ -0,0 +1,11 @@
+def menu_tournoi():
+    return input(
+        "Bienvenue sur le menu du tournoi \n"
+        "Que puis-je faire pour vous ? \n"
+        "Taper   pour ajouter un joueur \n "
+        "Taper   pour creer les matchs et  entrer les scores\n"
+        "Taper   pour montrer les resultats des matchs et les round \n"
+        "Taper   pour montrer le classement du tournoi \n"
+        "Taper   pour voir tout les joueurs du  par ordre alphebetique \n"
+        "Taper   pour voir tout les joueurs grace a leurs classements \n "
+    )
